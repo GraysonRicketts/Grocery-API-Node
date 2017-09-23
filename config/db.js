cc540b95be2bd2d69101b0ace5cb7f8d9f307d89
Grayson <grayson.ricketts@gmail.com>
2017-09-23 17:04:42 -0500

    Refactored mongoose error message and changed process exit to 1.

--- config/db.js
+++ config/db.js
@@ -8,21 +8,23 @@ if (process.env.NODE_ENV === 'test') {
 mongoose.connect(process.env.DB_HOST, { useMongoClient: true })
 
 mongoose.connection.on('error', (err) => {  
-    console.error('Mongoose default connection error: ' + err)
-    process.exit(0)
+    throwMongooseError(err)
 })
 
 mongoose.connection.on('disconnected', () => {  
-    console.error('Mongoose default connection disconnected')
-    process.exit(0)
+    throwMongooseError('Mongoose default connection disconnected')
 })
 
 // If the Node process ends, close the Mongoose connection 
 process.on('SIGINT', function() {  
     mongoose.connection.close(() => { 
-    console.error('Mongoose default connection disconnected through app termination')
-        process.exit(0)
+        throwMongooseError('Mongoose default connection disconnected through app termination SIGINT')
     })
 })
+
+function throwMongooseError(msg) {
+    console.error('Mongoose error: ' + msg)
+    process.exit(1)
+}
 
 mongoose.Promise = global.Promise
