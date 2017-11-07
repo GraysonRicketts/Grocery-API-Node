7b3fae1cf022e92b3755f7697857dadc2b45bd4f
GraysonRicketts <gricket@purdue.edu>
2017-11-06 20:43:36 -0500

    Fixed null user bug in password config

--- server/config/passport.js
+++ server/config/passport.js
@@ -5,16 +5,21 @@ import db from './../models'
 
 
 const LocalStrategy = require('passport-local').Strategy
+const passwordError = 'User and/or password incorrect'
 
 passport.use(new LocalStrategy({
         usernameField: 'email'
     }, (email, password, done) => {
         db.User.findOne({ email }).exec()
             .then((user) => {
+                if (!user) {
+                    return done(null, false, { message: passwordError })
+                }
+
                 checkUserPassword(password, user, done)
             })
             .catch((err) => {
-                return done(err)
+                done(err)
             })
     }))
 
@@ -34,10 +39,10 @@ function checkUserPassword(password, user, done) {
     bcrypt.compare(password, user.password)
         .then((res) => {
             if (!user || !res) {
-                return done(null, false, { message: 'User and/or password incorrect' })
+                return done(null, false, { message: passwordError })
             }
 
-            return done(null, user)
+            done(null, user)
         })
         .catch((err) => {
             done(err)
