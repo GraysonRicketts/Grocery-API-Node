83fbe74c12f467ba383f710cd5933c0a31dbb6a3
GraysonRicketts <gricket@purdue.edu>
2017-12-17 13:48:13 -0600

    Changed to using wait-for

--- seeds/seed-db.sh
+++ seeds/seed-db.sh
@@ -1,4 +1,4 @@
-./wait-for-it.sh mongodb:270127
+./wait-for mongodb:270127
 
 mongoimport --db mongodb --collection users --file users.json
 mongoimport --db mongodb --collection items --file items.json
