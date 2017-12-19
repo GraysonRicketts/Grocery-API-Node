cfaa8334a6c504cfc51cd161faf483486052734b
GraysonRicketts <gricket@purdue.edu>
2017-12-18 18:42:11 -0600

    Made app.js indentation more standard

--- server/app.js
+++ server/app.js
@@ -10,33 +10,32 @@ import passport from './config/passport'
 const RedisStore = require('connect-redis')(session)
 const app = express()
 
-app
 // Middleware
-    .use(cookieParser())
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({
-        extended: false
-    }))
-    .use(session({
-        secret: 'TODO: Move secret to more secure location',
-        store: new RedisStore({
-            host: 'redis',
-            port: 6379,
-            ttl: 60 * 24 * 7
-        }),
-        cookie: {
-            secure: false,
-            maxAge: 60 * 60 * 24 * 14
-        },
-        resave: false,
-        saveUninitialized: false
-    }))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(session({
+    secret: 'TODO: Move secret to more secure location',
+    store: new RedisStore({
+        host: 'redis',
+        port: 6379,
+        ttl: 60 * 24 * 7
+    }),
+    cookie: {
+        secure: false,
+        maxAge: 60 * 60 * 24 * 14
+    },
+    resave: false,
+    saveUninitialized: false
+}))
 
 // Authentication
-.use(passport.initialize())
-    .use(passport.session())
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Routes
-.use('/api', routes)
+app.use('/api', routes)
 
 export default app
