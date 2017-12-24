2307a332a0e89c87bbead3a6a8ab0b4e371a208f
GraysonRicketts <gricket@purdue.edu>
2017-12-24 12:31:54 -0600

    Made field optional

--- server/models/BasketItem.js
+++ server/models/BasketItem.js
@@ -4,14 +4,13 @@ import mongoose from 'mongoose'
 const { Schema } = mongoose
 
 const basketItemSchema = new Schema({
-  // TODO: Becuase this is never being saved it is never being validated
-  itemDef: { 
-    type: Schema.Types.ObjectId,
-    ref: 'Item',
-    required: true
-  },
-  quantity: { type: Number, required: true },
-  size: { type: String } // TODO: Expand to have multiple types of size or category list to choose from
+    itemDef: {
+        type: Schema.Types.ObjectId,
+        ref: 'Item',
+        required: true
+    },
+    quantity: { type: Number },
+    size: { type: String }
 })
 
 const BasketItem = mongoose.model('BasketItem', basketItemSchema)
